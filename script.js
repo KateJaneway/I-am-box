4b014486be6c22bf3eea189e68bfd292cdedf21c
KateJaneway <62800950+KateJaneway@users.noreply.github.com>
2020-03-29 16:45:28 -0400

    Unmodified exercise

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. Use document.querySelector to store the <div> in a variable called "box"
+
+
+// 2. Call the .onclick method on the variable from step one
+
+// 3. Assign the .onclick method to a function called makeBig that increases the height and width of the <div> to 600px each (hint: use .style.height and .style.width)
